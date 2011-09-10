ea35abdd66dd0f7c4246157910fecd6b9d8bb619
Rieethaa <rieethaa@gmail.com>
2011-09-10 19:41:26 +0100

    Added a temporary test to create a sample XML file called PostalAddressSample1.xml

--- XmlScannerApp/XmlScannerApp.Tests/XmlSerialisation.cs
+++ XmlScannerApp/XmlScannerApp.Tests/XmlSerialisation.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using XmlScannerApp.Models;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace XmlScannerApp.Tests
 {
@@ -10,9 +13,22 @@ namespace XmlScannerApp.Tests
 	public class XmlSerialisation
 	{
 		[Test]
-		public void DummyTest()
+		public void TemporaryTestToGenerateSampleXmlDocument()
 		{
+			var postalAddress = new PostalAddress();
+			postalAddress.PostalAddresses = new[]
+			{
+				new PostalAddressRecord { Address1 ="No. 10", Address2 = "Downing Street", Address3 = "", 
+					City = "London", Country = "UK", PostCode = "W1 1AA" },
+ 				new PostalAddressRecord { Address1 = "Andrews House" , Address2 = "College Road", Address3 = "", 
+					City = "Guildford", Country = "UK", PostCode = "GU1 4QB" }
+			};
 
+			var serializer = new XmlSerializer(typeof(PostalAddress));
+			using (var fileStream = new FileStream(@"..\..\..\XmlScannerApp\Data\PostalAddressSample1.xml", FileMode.Create))
+			{
+				serializer.Serialize(fileStream, postalAddress);
+			}
 		}
 	}
 }
