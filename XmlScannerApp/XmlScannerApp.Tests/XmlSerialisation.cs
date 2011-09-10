a5af4346d77cda39a835ec308f7a6dbff35d5323
Rieethaa <rieethaa@gmail.com>
2011-09-10 18:34:04 +0100

    Added a dummytest

--- XmlScannerApp/XmlScannerApp.Tests/XmlSerialisation.cs
+++ XmlScannerApp/XmlScannerApp.Tests/XmlSerialisation.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
 namespace XmlScannerApp.Tests
 {
+	[TestFixture]
 	public class XmlSerialisation
 	{
+		[Test]
+		public void DummyTest()
+		{
+
+		}
 	}
 }
