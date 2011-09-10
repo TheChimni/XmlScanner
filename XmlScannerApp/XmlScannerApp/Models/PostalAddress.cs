13d50aaea5f898d35a012dce71d55f72d21e1ec5
Rieethaa <rieethaa@gmail.com>
2011-09-10 19:16:21 +0100

    Added PostalAddress Model to the project auto gerenated originally using XSD.exe

---
+++ XmlScannerApp/XmlScannerApp/Models/PostalAddress.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Serialization;
+
+namespace XmlScannerApp.Models
+{
+	[XmlType(AnonymousType = true, Namespace = "urn:PostalAddress-schema")]
+	[XmlRoot(Namespace = "urn:PostalAddress-schema", IsNullable = false)]
+	public class PostalAddress
+	{
+		[XmlElement("PostalAddressRow")]
+		public PostalAddressRecord[] PostalAddresses { get; set; }
+	}
+	
+	[XmlType(Namespace = "urn:PostalAddress-schema")]
+	public class PostalAddressRecord
+	{
+		[XmlElement("address1")]
+		public string Address1 { get; set; }
+		[XmlElement("address2")]
+		public string Address2 { get; set; }
+		[XmlElement("address3")]
+		public string Address3 { get; set; }
+		[XmlElement("city")]
+		public string City { get; set; }
+		[XmlElement("postcode")]
+		public string PostCode { get; set; }
+		[XmlElement("country")]
+		public string Country { get; set; }
+	}
+}
