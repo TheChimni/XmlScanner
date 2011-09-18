c9f5d044bae893ac75f73c2547bdf25fd2aabd8d
Rieethaa <rieethaa@gmail.com>
2011-09-18 23:27:02 +0100

    added the web address reader

---
+++ XmlScannerApp/XmlScannerApp/Models/WebAddressReader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using XmlScannerApp.Models;
+using System.Configuration;
+
+namespace XmlScannerApp.Models
+{
+	public class WebAddressReader
+	{
+		private const string SchemaFileName = "WebAddress.xsd";
+		private const string XmlFolder = "WebAddresses";
+		private string DataFolder { get; set; }
+
+		public WebAddressReader(string dataFolder)
+		{
+			DataFolder = dataFolder;
+		}
+
+	}
+}
