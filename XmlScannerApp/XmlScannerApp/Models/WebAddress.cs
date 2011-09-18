682cc20bf0e7d9b400ea08fc9eefb6764ec5eae0
Rieethaa <rieethaa@gmail.com>
2011-09-18 20:35:38 +0100

    Added a web address.cs class generated from the XSD tool and some fiddling

---
+++ XmlScannerApp/XmlScannerApp/Models/WebAddress.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Serialization;
+
+namespace XmlScannerApp.Models
+{
+	[XmlType(AnonymousType = true, Namespace = "urn:WebAddress-schema")]
+	[XmlRoot(Namespace = "urn:WebAddress-schema", IsNullable = false)]
+	public class WebAddress
+	{
+		[XmlElement("WebAddressRow")]
+		public WebAddressRecord[] WebAddresses { get; set; }
+	}
+
+	[XmlType(Namespace = "urn:WebAddress-schema")]
+	public class WebAddressRecord
+	{
+		[XmlElement("url")]
+		public string Url { get; set; }
+		[XmlElement("description")]
+		public string Description { get; set; }
+		[XmlElement("favicon")]
+		public string Favicon { get; set; }
+		[XmlElement("tags")]
+		public string Tags { get; set; }
+	}
+}
